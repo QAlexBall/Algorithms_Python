ea45d4fe0004461fdad7071296525c4ffd6884c5
QAlexBall <zhuderen12345@icloud.com>
2018-08-21 15:07:37 +0800

    update

--- Usual/TwoSum.py
+++ Usual/TwoSum.py
@@ -7,13 +7,16 @@ class Solution:
         """
         dic = {}
         for i, num in enumerate(nums):
-            if(target - num) in dic:
+            if (target - num) in dic:
                 return dic[target - num], i
             dic[num] = i
+
 
 def main():
     nums = [2, 7, 11, 13]
     target = 9
     print(Solution().twoSum(nums, target))
+
+
 if __name__ == '__main__':
     main()
