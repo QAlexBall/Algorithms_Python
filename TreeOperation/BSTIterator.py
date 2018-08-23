69d71c77f343b04ff05162eda0aa3848d161f720
QAlexBall <zhuderen12345@icloud.com>
2018-08-23 18:48:19 +0800

    update

--- TreeOperation/BSTIterator.py
+++ TreeOperation/BSTIterator.py
@@ -3,17 +3,28 @@ class BSTIterator(object):
         """
         :type root: TreeNode
         """
+        self.stack = []
 
+        def inOrder(root):
+            if not root:
+                return
+            inOrder(root.right)
+            self.stack.append(root.val)
+            inOrder(root.left)
+
+        inOrder(root)
 
     def hasNext(self):
         """
         :rtype: bool
         """
+        return len(self.stack) > 0
 
     def next(self):
         """
         :rtype: int
         """
+        return self.stack.pop()
 
 # Your BSTIterator will be called like this:
 # i, v = BSTIterator(root), []
